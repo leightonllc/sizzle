f116a0e289e4d683f8f35e851d02518c0ae08de7
leightonllc <leightonllc@outlook.com>
2022-02-04 18:26:01 +0800

    Game skeleton finished

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
 let $card = document.getElementsByClassName("card");
-let $keyboard = document.getElementsByClassName("keyboard");
-var elements = document.getElementsByClassName("classname");
+let $input = document.getElementsByClassName("input");
+let $enter = document.getElementsByClassName("enter");
+let answer = ["1", "2", "3", "4", "5", "6"];
 
-var mouseclick = function() {
+
+
+let arr = [];
+let m = 0, n = 0;
+
+var inputmouseclick = function() {
     var attribute = this.innerHTML;
-    alert(attribute);
+	if (arr.length < 6) {
+		arr.push(attribute);
+		$card[m * 6 + n].innerHTML = attribute;
+		n++;
+	}
+    //alert(arr);
 };
 
-for (var i = 0; i < $keyboard.length; i++) {
-    $keyboard[i].addEventListener('click', mouseclick, false);
+var entermouseclick = function() {
+    for (var i = 0; i < 6; i++){
+		if (answer[i] == $card[m * 6 + i].innerHTML) {
+			$card[m * 6 + i].classList.add("card-green");
+		} else 
+			if (answer.includes($card[m * 6 + i].innerHTML)) {
+				$card[m * 6 + i].classList.add("card-red");
+			} else {
+				$card[m * 6 + i].classList.add("card-black");
+			}
+		//alert(answer.includes($card[m * 6 + i].innerHTML));
+		
+	}
+	m++;
+	n = 0;
+	arr = [];
+    
+};
+
+for (var i = 0; i < $input.length; i++) {
+    $input[i].addEventListener('click', inputmouseclick, false);
 }
+
+$enter[0].addEventListener('click', entermouseclick, false);
